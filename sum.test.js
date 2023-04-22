8513ee90866fa65daf33e7f6dd89a73231a1b3dd
atardecer01 <rivas.lina@correounivalle.edu.co>
2023-04-21 18:30:52 -0600

    Initial commit with sum function and tests3.0

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,9 @@
 const sum = require('./sum');
+const mul = require('./mult');
+const div = require('./div');
 
 test('sums two numbers', () => {
   expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe(2);
+  expect(div(2, 2)).toBe(1);
 });
